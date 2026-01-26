eb46f1142e3c056089bb8bffd88ce89208c1e6e7
Ed Baker <edwbaker@gmail.com>
2026-01-26 11:23:52 +0000

    Add software_version field to JSON output

--- src/main.c
+++ src/main.c
@@ -176,6 +176,7 @@ static void build_sensor_json(char *output, size_t output_len,
     ws_json_replace_null_bool(output, "internal", internal);
     snprintf(timestamp_str, sizeof(timestamp_str), "%ld", (long)timestamp);
     ws_json_replace_null_string(output, "timestamp", timestamp_str);
+    ws_json_replace_null_string(output, "software_version", VERSION);
     if (error_msg) {
         char escaped_error[256];
         ws_json_escape_string(error_msg, escaped_error, sizeof(escaped_error));
